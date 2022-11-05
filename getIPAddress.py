f54bac1ff5da05f0d3900be862f584649340607c
NetAcad DEVASC <changeme@example.com>
2022-11-05 10:21:40 +0000

    test

--- getIPAddress.py
+++ getIPAddress.py
@@ -2,15 +2,39 @@
 #requests library in order to get API from the website 
 from requests import get
 
-#run API from ipify.org and convert results to text
-#API takes Public IPv4 Address of Computer
-ip = get('https://api.ipify.org').text
+while True:
+    #get user input
+    print('(1) Use own IP address')
+    print('(2) Manually enter IP address')
+    print('(3) Exit')
+    choice = input('Enter number of choice: ')
 
-#print Public IPv4 Address
-print('My public IP address is: {}'.format(ip))
+    if choice=='1':
+        ip = get('https://api.ipify.org').text
 
-#run API from ipapi.co
-#API takes an input of an IPv4 Address and gets information
-#based off of it
-loc = get('https://ipapi.co/'+ip+'/json/')
-print (loc.json())
+        #print Public IPv4 Address
+        print('My public IP address is: {}'.format(ip))
+
+        #run API from ipapi.co
+        #API takes an input of an IPv4 Address and gets information
+        #based off of it
+        loc = get('https://ipapi.co/'+ip+'/json/')
+        print (loc.json())
+        print()
+        print()
+    elif choice=='2':
+        ip = input('Enter IPv4 Address: ')
+        print('The IPv4 Address you inputted is ' + ip)
+        loc = get('https://ipapi.co/'+ip+'/json/')
+        print (loc.json())
+        print()
+        print()
+    elif choice=='3':
+        print()
+        print('Thank you for using our application')
+        print()
+        break
+    else:
+        print('Invalid input, please try again.')
+        print()
+        print()
